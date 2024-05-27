f8ca5bcf9022b11fe38421ddab5ab44b80682055
Houda202 <info.houda@gmail.com>
2024-05-27 19:31:19 +0100

    Mise à jour du site web avec les dernières modifications

---
+++ assets/plugins/script.js
@@ -0,0 +1,10 @@
+// script.js
+function translateToFrench() {
+    document.getElementById("headline").innerText = "Bienvenue à Prestige Voyage";
+    // Ajoutez d'autres éléments à traduire ici
+}
+
+function translateToEnglish() {
+    document.getElementById("headline").innerText = "Welcome to Prestige Voyage";
+    // Ajoutez d'autres éléments à traduire ici
+}
